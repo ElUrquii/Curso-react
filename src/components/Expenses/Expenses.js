30447ac68179f9ef170809573152df1c73b1f9b6
ElUrquii <urquizamaxi099@gmail.com>
2021-05-18 10:52:29 -0300

    Dinamic component rendering

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -12,26 +12,18 @@ function Expenses(props) {
 
   return (
     <div>
-      <ExpenseFilter
-        selectedValue={filterValue}
-        onFilterChange={filterChangeHandler}
-      />
       <Card className="expenses">
-        <ExpenseItem
-          title={props.expenses[0].title}
-          amount={props.expenses[0].amount}
-          date={props.expenses[0].date}
-        ></ExpenseItem>
-        <ExpenseItem
-          title={props.expenses[2].title}
-          amount={props.expenses[2].amount}
-          date={props.expenses[2].date}
-        ></ExpenseItem>
-        <ExpenseItem
-          title={props.expenses[3].title}
-          amount={props.expenses[3].amount}
-          date={props.expenses[3].date}
-        ></ExpenseItem>
+        <ExpenseFilter
+          selectedValue={filterValue}
+          onFilterChange={filterChangeHandler}
+        />
+        {props.expenses.map((expense) => (
+          <ExpenseItem
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        ))}
       </Card>
     </div>
   );
